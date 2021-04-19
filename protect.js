1a057b47a92c63c7a36a26c4436f39bf210eebe2
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-04-19 21:15:06 +0700

    Update protect.js

--- protect.js
+++ protect.js
@@ -28,7 +28,7 @@ var exception = exception();
 var idsafe = new Array();
 idsafe[0] = 'https://www.blogger.com/feeds/6559145272696448190/posts/default?alt=json-in-script&max-results=10&callback=showurl';
 idsafe[1] = 'https://www.blogger.com/feeds/3435246800074833531/posts/default/-/News?alt=json-in-script&max-results=10&callback=showurl';
-var getrandomid = Math.round(Math.random() * 2);
+var getrandomid = Math.round(Math.random() * 1);
 var linksafe = idsafe[getrandomid];
 loadScript(linksafe);
 function loadScript(linksafe) {

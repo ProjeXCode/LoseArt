6c5a7f43d53cb0a493e1ea18fc1246ba00f36eef
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-05-03 13:10:59 +0700

    Create safety.js

---
+++ safety.js
@@ -0,0 +1,13 @@
+    var idsafe = new Array();
+idsafe[0] = 'https://www.blogger.com/feeds/6559145272696448190/posts/default?alt=json-in-script&max-results=10&callback=showurl';
+idsafe[1] = 'https://www.blogger.com/feeds/3435246800074833531/posts/default/-/News?alt=json-in-script&max-results=150&callback=showurl';
+var getrandomid = Math.round(Math.random() * 1);
+var linksafe = idsafe[getrandomid];
+loadScript(linksafe);
+function loadScript(linksafe) {
+    var oHead = document.getElementsByTagName("HEAD")[0];
+    var oScript = document.createElement('script');
+    oScript.type = 'text/javascript';
+    oScript.src = linksafe;
+    oHead.appendChild(oScript);
+}

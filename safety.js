a7aea39a029f3181e67a80f3931d36c41a5abf58
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-05-11 10:41:30 +0700

    Update safety.js

--- safety.js
+++ safety.js
@@ -1,6 +1,7 @@
     var idsafe = new Array();
 idsafe[0] = 'https://www.blogger.com/feeds/3435246800074833531/posts/default/-/News?alt=json-in-script&max-results=150&callback=showurl';
-var getrandomid = Math.round(Math.random() * 0);
+idsafe[1] = 'https://www.blogger.com/feeds/3435246800074833531/posts/default/-/News?alt=json-in-script&max-results=150&callback=showurl';
+var getrandomid = Math.round(Math.random() * 1);
 var linksafe = idsafe[getrandomid];
 loadScript(linksafe);
 function loadScript(linksafe) {

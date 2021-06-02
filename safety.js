92974cc87e30f0b7433692f6650472a97250d05a
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-06-02 12:25:19 +0700

    Update safety.js

--- safety.js
+++ safety.js
@@ -1,7 +1,7 @@
     var idsafe = new Array();
-idsafe[0] = 'https://www.blogger.com/feeds/3891678444866704785/posts/default?alt=json-in-script&max-results=10&callback=showurl';
-idsafe[1] = 'https://www.blogger.com/feeds/6559145272696448190/posts/default?alt=json-in-script&max-results=10&callback=showurl';
-idsafe[2] = 'https://www.blogger.com/feeds/5993611139216363123/posts/default?alt=json-in-script&max-results=10&callback=showurl';
+idsafe[0] = 'https://www.blogger.com/feeds/3891678444866704785/posts/default?alt=json-in-script&max-results=150&callback=showurl';
+idsafe[1] = 'https://www.blogger.com/feeds/6559145272696448190/posts/default?alt=json-in-script&max-results=150&callback=showurl';
+idsafe[2] = 'https://www.blogger.com/feeds/5993611139216363123/posts/default?alt=json-in-script&max-results=150&callback=showurl';
 var getrandomid = Math.round(Math.random() * 2);
 var linksafe = idsafe[getrandomid];
 loadScript(linksafe);

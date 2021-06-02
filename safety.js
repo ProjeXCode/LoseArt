2159019cb1d89fdbbb438397f61e1c258cd01d0b
ProjeXCode <24662855+ProjeXCode@users.noreply.github.com>
2021-06-02 15:08:21 +0700

    Update safety.js

--- safety.js
+++ safety.js
@@ -1,4 +1,5 @@
     var idsafe = new Array();
+idsafe[0] = 'https://www.blogger.com/feeds/3891678444866704785/posts/default?alt=json-in-script&max-results=150&callback=showurl';
 idsafe[1] = 'https://www.blogger.com/feeds/3891678444866704785/posts/default?alt=json-in-script&max-results=150&callback=showurl';
 idsafe[2] = 'https://www.blogger.com/feeds/6559145272696448190/posts/default?alt=json-in-script&max-results=150&callback=showurl';
 idsafe[3] = 'https://www.blogger.com/feeds/5993611139216363123/posts/default?alt=json-in-script&max-results=150&callback=showurl';
